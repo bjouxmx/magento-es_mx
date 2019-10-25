7652ecadc393cd33e3103fc964c368d6a6d6a059
bjouxmx <56979227+bjouxmx@users.noreply.github.com>
2019-10-24 22:49:53 -0500

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Magento 2 Spanish (Mexico) language pack
+ *
+ * @category   Language
+ * @package    es_MX
+ * @author     fasaro
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'fasaro_es_mx',
+    __DIR__
+);
