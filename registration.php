4f0878c2e49c205e42d829ae37f9e6c3bfb20367
bjouxmx <56979227+bjouxmx@users.noreply.github.com>
2019-10-24 22:52:19 -0500

    Update registration.php

--- registration.php
+++ registration.php
@@ -8,6 +8,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'fasaro_es_mx',
+    'bjouxmx_es_mx',
     __DIR__
 );
